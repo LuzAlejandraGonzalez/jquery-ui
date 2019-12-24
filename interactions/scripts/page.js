53a19f3c8a6941f776b51b136ae6beeb41fca894
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2019-12-23 21:47:08 -0700

    Finished Module Interactions with jQuery

--- interactions/scripts/page.js
+++ interactions/scripts/page.js
@@ -1,3 +1,35 @@
 ﻿/// <reference path="jquery-1.6.2.js" />
 /// <reference path="jquery-ui-1.8.16.custom.js" />
 
+$(function(){
+   $("#d1").draggable({
+       revert: "invalid"
+    });
+
+   $("#d2").draggable({
+       snap: "#d1, #d3",
+       helper: function(){
+           return $("<div>I am a helper</div>");
+
+       }
+    });
+
+   $("#d3").draggable({
+      
+       handle: "header"
+    });
+
+    $("#trash").droppable({
+        activeClass: "opaque",
+        accept: "#d3",
+        drop: function(event, ui) {
+            ui.draggable.fadeOut(function() {
+                ui.draggable.remove();
+            });
+        }
+    });
+
+    $("#sortable").sortable({ placeholder: "placeholder" });
+
+    $("#d3").resizable({ aspectRatio: true, alsoResize: "#d2" });
+});
