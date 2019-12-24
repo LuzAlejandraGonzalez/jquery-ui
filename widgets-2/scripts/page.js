2b6711f5a11497168b0c65530c88f5f6b082b423
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2019-12-23 19:07:27 -0700

    Finished Module Widgets Part 2

--- widgets-2/scripts/page.js
+++ widgets-2/scripts/page.js
@@ -53,6 +53,54 @@ $(function () {
 
     $("#tabs").tabs();
 
-    
+    $("#accordian").accordion({ autoHeight: false, collapsible:true,
+        change: function(event, ui){
+            console.log(event);
+            console.log(ui);
+        }
+    });
+
+    $("#accordian").accordion("activate",false);
+
+    var classes = [
+        "C#",
+        "Entity Framework",
+        "LINQ",
+        "ASP.NET MVC",
+        "jQuery UI",
+        "CSS",
+        "Ruby",
+        "jQuery",
+        "JavaScript",
+        "Test driven development",
+        "Design patterns"
+    ];
+
+    $("#search").autocomplete({
+        source: "autocompletejson.ashx",
+        minLength: 2,
+        delay: 1500
+    });
+
+    $("#buttons").children()
+    .button({ icons: { primary: "ui-icon-search", secondary: "ui-icon-wrench"} })
+    .click(function () {
+        alert($(this).value);
+    });
+
+    $("#radios").buttonset();
+    $("#checks").buttonset();
+
+    $("#dateselection").datepicker({
+    showAnim: "bounce",
+    numberOfMonths: 2,
+    showWeek: true,
+    changeMonth: true,
+    changeYear: true,
+    showButtonPanel: true,
+    minDate: new Date(2008, 1 - 1, 1),
+    maxDate: new Date(2012, 12 - 1, 31)
+    });
+    $("#dateselection").datepicker("setDate", new Date(2011, 6 - 1, 11));
 
 });
