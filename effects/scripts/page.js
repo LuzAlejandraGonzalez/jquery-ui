7eb1bc7dbaa70581db9df5ce4cb966951d2679ec
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2019-12-23 22:37:48 -0700

    Finished Module jQuery UI Effects

--- effects/scripts/page.js
+++ effects/scripts/page.js
@@ -2,5 +2,19 @@
 /// <reference path="jquery-ui-1.8.16.custom.js" />
 
 $(function () {
-    
+
+    $("#target")
+        .css("position", "relative")
+        .click(function () {
+            $(this).toggleClass("leftTarget", "slow");
+        });
+
+    $("#draggables").children().draggable();
+
+    $("#trash").droppable({
+        activeClass: "opaque",
+        drop: function (event, ui) {
+            ui.draggable.hide("explode");
+        }
+    });
 });
